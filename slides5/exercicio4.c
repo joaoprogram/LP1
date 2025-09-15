52102c0790d0167a4acbcecf9f79897b1d241e1e
João Gabriel Melo <joaogabriel.s.melo92@gmail.com>
2025-09-14 22:25:34 -0300

    Create exercicio4.c

---
+++ slides5/exercicio4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int troca(int *p1, int *p2) {
+    int temp = *p1; 
+    *p1 = *p2;      
+    *p2 = temp;      
+}
+
+int main(void) {
+    int x = 10, y = 20;
+
+    troca(&x, &y);
+
+    printf("%d %d\n", x, y);
+
+    return 0;
+
+}
