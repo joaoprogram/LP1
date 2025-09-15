faa81d74f56b5c76a880b55240ba72f9e76a2882
João Gabriel Melo <joaogabriel.s.melo92@gmail.com>
2025-09-14 22:46:30 -0300

    Update exercicio01.c

--- slides6vetores/exercicio01.c
+++ slides6vetores/exercicio01.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+void preenche(int *xs, int n){
+    for (int i = 0; i < n; i++){
+        scanf("%d", (xs + i));
+    }
+}
+
+float media(int *xs, int n){
+    int soma = 0;
+    for (int i = 0; i < n; i++){
+        soma += *(xs + i);
+    }
+    return (float)soma / n;
+}
+
+int main(void){
+    int vec[100];
+    int n;
+
+    printf("Digite a quantidade de elementos: ");
+    scanf("%d", &n);
+
+    preenche(vec, n);
+
+    float m = media(vec, n);
+
+    printf("Media = %.2f\n", m);
+
+    return 0;
+}
